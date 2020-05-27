4fcda7fa34d7aa9fe67be9e37ba249514610640a
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-05-27 12:14:28 -0700

    Movement with arrowkeys

--- Assets/Scripts/SpriteMovement.cs
+++ Assets/Scripts/SpriteMovement.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class SpriteMovement : MonoBehaviour
 {
+    //Gameobject Placeholder
+    private Transform tf;
+    //Setting Gameobject movement speed
     public float moveSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Getting the gameobject location
+        tf = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        //Gameobject movement with arrow keys
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Vector3.up * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        //Sets the gameobject to the origin
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tf.position = new Vector3(0, 0, 0);
+        }
     }
 }
